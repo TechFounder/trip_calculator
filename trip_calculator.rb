5f5d5e063e1afa2875d755d9db8ed7b04169fc1d
TechFounder <techfounder@outlook.com>
2013-05-21 23:36:20 -0400

    Last update

--- trip_calculator.rb
+++ trip_calculator.rb
@@ -1,8 +1,3 @@
-def printstatement(hour, cost)
-	puts "You trip will take #{hours} hours and cost $#{cost}"8
-end
-
-
 print "How far are you going (miles)? "
 distance = gets.chomp.to_f
 
@@ -27,37 +22,13 @@ if speed > 60
 
 	cost = (cost_per_gallon / penalty_miles_per_gallon) * distance
 
-	printstatement
+	puts "You trip will take #{hours} hours and cost $#{cost}"
 
 else
 
 	hours = distance / speed
 	cost = (cost_per_gallon / miles_per_gallon) * distance
 
-	printstatement
-
-end
-
-
-
-
-
-
-
-def getUserInput
-
-	print "How far are you going (miles)? "
-	distance = gets.chomp.to_f
-
-	print "what milage are you getting (mpg)? "
-	miles_per_gallon = gets.chomp.to_f
-
-	print "What's the cost of gas ($)? "
-	cost_per_gallon = gets.chomp.to_f
-
-	print "What speed are you traveling (mph)? "
-	speed = gets.chomp.to_f
-
-	return distance, miles_per_gallon, cost_per_gallon, speed
+	puts "You trip will take #{hours} hours and cost $#{cost}"
 
 end
